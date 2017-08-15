0eb05a93970abfc9b35e3d81b41ce318f0d79c86
HefeiQiu <qiuhefei@gmail.com>
2017-08-15 15:33:26 -0400

    fixed data path and text for LDA

--- prepare.py
+++ prepare.py
@@ -1,20 +1,31 @@
 """
 Prepare data
 """
+import argparse
 import pandas as pd
 import numpy as np
 from datetime import datetime
-import matplotlib.pylab as plt
 
-file_path = './北美留学生.xlsx'
-train_set = 0.8 # Percentage of training set
-valid_set = 0.1 # Percentage of validation set
-test_set = 0.1 # Percentage of test set
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data', default='./data/', help='output file for the prepared data')
+parser.add_argument('--raw_data', default='北美留学生.xlsx', help='file name of raw data')
+parser.add_argument('--train_percent', default=0.8, help='percentage of training set')
+parser.add_argument('--valid_percent', default=0.1, help='percentage of validation set')
+parser.add_argument('--test_percent', default=0.1, help='percentage of test set')
+parser.add_argument('--lda', default='True', help='preparing data for LDA model')
+opt = parser.parse_args()
+print(opt)
+"""
+train_set = 0.8
+valid_set = 0.1
+test_set = 0.1
 lda = True # LDA model
+"""
 
 # Save dataframe to .txt file
 def saveToTxt(df, file_name):
-    df.to_csv(file_name, header=None, index=None, sep=' ')
+    df.to_csv(opt.data + file_name, header=None, index=None, sep=' ')
 
 
 # Get xs and ys (columns '标题', '正文', '点赞数')
@@ -25,12 +36,12 @@ def getData(df):
 
 print('Start to prepare data...')
 # Read excel file to dataframe
-df = pd.read_excel(file_path, sheetname='list')
+df = pd.read_excel(opt.data + opt.raw_data, sheetname='list')
 # Clean data
 df.dropna(axis=0, how='any', subset=['正文','标题'], inplace=True)
 
 # Prepare data for LDA
-if lda:
+if opt.lda:
     print('Prepare data for LDA...')
 
     # Extract year, month
@@ -39,13 +50,15 @@ if lda:
     df['月'] = df['发布时间'].dt.month
     for i in range(np.min(df['年']), np.max(df['年'])+1):
         for j in range(np.min(df['月']), np.max(df['月'])+1):
-            df_body = df['正文'][(df['年']==i) & (df['月']==j)]
+            df_body = df[['标题','正文']][(df['年']==i) & (df['月']==j)]
             if not (df_body.empty):
                 saveToTxt(df_body, str(i)+'_'+str(j)+'.txt')
 
 # Split data into training, validation, test data sets, psedudo randomized
 train, validate, test = np.split(df.sample(frac=1, random_state=123), 
-                                 [int(train_set*len(df)), int((train_set + valid_set)*len(df))])
+                        [int(opt.train_percent*len(df)), \
+                         int((opt.train_percent + opt.valid_percent)*len(df))])
+
 
 title_train, body_train, y_train = getData(train)
 title_validate, body_validate, y_validate = getData(validate)
